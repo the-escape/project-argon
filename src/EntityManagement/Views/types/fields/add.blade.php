c171e6be870dca9f4f9c0c36f0d6e62cfc67882b
Pawel Nowak <pablo.novacco@gmail.com>
2015-10-06 11:49:16 +0100

    Updated add field layout. Added missing back buttons.

--- src/EntityManagement/Views/types/fields/add.blade.php
+++ src/EntityManagement/Views/types/fields/add.blade.php
@@ -48,6 +48,9 @@
                 </div>
             </div>
             <button type="submit" class="btn btn-primary">Save</button>
+            <a class="btn btn-link" href="{{route('cms:types:edit', [$type->id])}}">Back to edit type</a>
+            <a class="btn btn-link" href="{{route('cms:types:manage')}}">Back to types</a>
         </form>
+
     </div>
 @endsection
