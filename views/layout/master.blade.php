9a89710ab67920d7da1cb8ff144afc6da9e921e4
Pawel Nowak <pablo.novacco@gmail.com>
2016-05-10 14:30:50 +0100

    Fixed and refactored wysiwyg js stuff.

--- views/layout/master.blade.php
+++ views/layout/master.blade.php
@@ -121,14 +121,14 @@
     <?php
     // enable ajax post requests as per http://laravel.com/docs/master/routing#csrf-x-csrf-token ?>
     $.ajaxSetup({
-                headers: {
-                    'X-CSRF-TOKEN': "<?=csrf_token();?>"
-                }
-            });
+        headers: {
+            'X-CSRF-TOKEN': "<?=csrf_token();?>"
+        }
+    });
 
-            <?php
-            // ACCORDIONS: Handle all accordion instances on the page with .accordion-expand-collapse trigger ?>
-            var $accordionExpandCollapse = $('.accordion-expand-collapse');
+    <?php
+    // ACCORDIONS: Handle all accordion instances on the page with .accordion-expand-collapse trigger ?>
+    var $accordionExpandCollapse = $('.accordion-expand-collapse');
 
     $accordionExpandCollapse.click(function()
     {
@@ -150,284 +150,137 @@
     <?php
     // ACCORDIONS: Handle individial accordions ?>
     $('.accordion').accordion(
+    {
+        active: false,
+        header: ".accordion-header",
+        collapsible: true,
+        heightStyle: "content",
+        icons: {
+            activeHeader: "accordion-header-open",
+            header: "accordion-header-close"
+        },
+        animate: {
+            duration: 400
+        },
+        activate: function()
+        {
+            var isActive = $(this).accordion("option", "active");
+
+            if (isActive === false)
             {
-                active: false,
-                header: ".accordion-header",
-                collapsible: true,
-                heightStyle: "content",
-                icons: {
-                    activeHeader: "accordion-header-open",
-                    header: "accordion-header-close"
-                },
-                animate: {
-                    duration: 400
-                },
-                activate: function()
+                $accordionExpandCollapse.each(function()
                 {
-                    var isActive = $(this).accordion("option", "active");
+                    var $self = $(this);
+                    var expandAllText = $self.data('data-expand') || 'Expand all';
 
-                    if (isActive === false)
+                    if(!$('.accordion-header.ui-state-active').length)
                     {
-                        $accordionExpandCollapse.each(function()
-                        {
-                            var $self = $(this);
-                            var expandAllText = $self.data('data-expand') || 'Expand all';
-
-                            if(!$('.accordion-header.ui-state-active').length)
-                            {
-                                $self.removeClass('expanded');
-                            }
-
-                            $self.text(expandAllText);
-                            $self.removeAttr('data-expanded');
-                        });
+                        $self.removeClass('expanded');
                     }
-                    else
-                    {
-                        $accordionExpandCollapse.each(function()
-                        {
-                            var $self = $(this);
-                            var collapseAllText = $self.data('data-collapse') || 'Collapse all';
 
-                            $self.addClass('expanded');
-                            $self.text(collapseAllText);
-                            $self.attr('data-expanded', true);
-                        });
-                    }
-                }
-            });
+                    $self.text(expandAllText);
+                    $self.removeAttr('data-expanded');
+                });
+            }
+            else
+            {
+                $accordionExpandCollapse.each(function()
+                {
+                    var $self = $(this);
+                    var collapseAllText = $self.data('data-collapse') || 'Collapse all';
+
+                    $self.addClass('expanded');
+                    $self.text(collapseAllText);
+                    $self.attr('data-expanded', true);
+                });
+            }
+        }
+    });
 
 
     <?php
     // ACCORDIONS: expand all instances on load after slight delay. ?>
     setTimeout(function(){
-                //$accordionExpandCollapse.trigger('click');
-            }, 0);
+        //$accordionExpandCollapse.trigger('click');
+    }, 0);
 
-
-    <?php
-    // FIELDS CLONING: based on data attr, allows to move around the 'clone' button, since data-clone attr reference. ?>
-    $(document).on('click', '.field-clone', function()
-            {
-                // Disabled prior to removal.
-                // No longer used for cloning text, select or combo fields. Wysiwyg still outstanding.
-                return;
-                var $self = $(this);
-                var $parentFormGroup = $self.closest('.form-group');
-
-                if ($parentFormGroup && $parentFormGroup.length)
-                {
-                    var $el = $parentFormGroup.children('.form-control, .input-group').first();
-                    var isInputGroup = $el.hasClass('input-group');
-
-                    if (typeof this.dataset.field !== 'undefined')
-                    {
-                        var field = parseInt(this.dataset.field, 10);
-
-                        if (!isNaN(field))
-                        {
-                            if(window.console) console.log("Firing ajax...");
-
-                            var postdata = {};
-
-                            if (typeof this.dataset.hash !== 'undefined')
-                            {
-                                postdata.hash = this.dataset.hash;
-                            }
-
-                            $.post("/admin/clone/" + field, postdata, function(){ if(window.console) console.log('POSTED...'); })
-                                    .done(function(data) {
-                                        if(window.console) console.log('Data returned:');
-                                        if(window.console) console.log($(data));
-
-                                        $self.before($(data));
-
-                                        // notify all observers
-                                        $.publish('field/clone', {'id':field});
-
-                                        // force all wysiwyg fields to populate native equivalents and remove before cloning
-                                        for (var i in CKEDITOR.instances)
-                                        {
-                                            CKEDITOR.instances[i].updateElement();
-                                            CKEDITOR.instances[i].destroy();
-                                        }
-                                        $('.ckeditor').each(function(i, el)
-                                        {
-                                            CKEDITOR.config.toolbar = getWysiwygToolbarOptions(el);
-                                            CKEDITOR.config.height = getWysiwygHeight(el);
-                                            CKEDITOR.config.format_tags = getWysiwygFormatTagsOptions(el);
-                                            CKEDITOR.config.on = {
-                                                'instanceReady': function(evt)
-                                                {
-//                                            if (el.id == field) // set the focus to cloned editor
-//                                            {
-//                                                this.focus();
-//                                            }
-                                                }
-                                            };
-                                            CKEDITOR.replace(el, CKEDITOR.config); // initialize manually with custom config
-                                        });
-                                    })
-                                    .fail(function() {
-                                        if(window.console) console.log('Failed while getting data.');
-                                    })
-                                    .always(function() {
-                                        if(window.console) console.log("Finished getting data.");
-                                    });
-                        }
-
-                        return false;
-                    }
-
-//                    var $elInput = (isInputGroup) ? $el.find('.form-control') : $el // find input field within cloned html
-//
-//                    var isWysiwyg = $elInput.hasClass('ckeditor');
-//
-//                    if (isWysiwyg)
-//                    {
-//                        // force all wysiwyg fields to populate native equivalents and remove before cloning
-//                        for (var i in CKEDITOR.instances)
-//                        {
-//                            CKEDITOR.instances[i].updateElement();
-//                            CKEDITOR.instances[i].destroy();
-//                        }
-//                    }
-//
-//                    var $cloned = $el.clone(true, true); // clone element
-//                    var $clonedInput = (isInputGroup) ? $cloned.find('.form-control') : $cloned // find input field within cloned html
-//                    var matches = $clonedInput.attr('name').match(/fields\[(\d+)\]/); // get field value by running a regex match
-//                    if (matches) {
-//                        $clonedInput.prop('name', 'fields[' + matches[1]+ '][]'); // update cloned name
-//                    } else {
-//                        var matches = $clonedInput.attr('name').match(/combo\[(\d+)\]\[(\d+)\]\[fields\]\[(\d+)\]/); // get field value by running a regex match
-//                        $clonedInput.prop('name', 'combo[' + matches[1]+ '][' + matches[2]+ '][fields][' + matches[3]+ '][]'); // update cloned name
-//                    }
-//                    $clonedInput.val('').removeAttr('value'); // clear cloned value
-//                    $clonedInput.removeClass('error'); // remove error class if exists from cloned element
-//
-//                    if (isWysiwyg)
-//                    {
-//                        var ID = new Date().getTime();
-//                        $clonedInput.attr('id', ID); // add generated ID, just to satisfy sortable on wysiwyg
-//                    }
-//                    else
-//                    {
-//                        $clonedInput.removeAttr('id');
-//                    }
-//
-//                    if (isInputGroup)
-//                    {
-//                        // insert cloned element after last of the same type. Note, copied one may be moved with sortable, so can't just insert after
-//                        $parentFormGroup.children('.input-group').last().after($cloned).next().find('.form-control').focus();
-//                    }
-//                    else
-//                    {
-//                        // insert cloned element after last of the same type. Note, copied one may be moved with sortable, so can't just insert after
-//                        $parentFormGroup.children('.form-control').last().after($cloned).next('.form-control').focus();
-//                    }
-//
-//                    if (isWysiwyg)
-//                    {
-//                        // rebuild all wysiwyg fields
-//                        $('.ckeditor').each(function(i, el)
-//                        {
-//                            CKEDITOR.config.toolbar = getWysiwygToolbarOptions(el);
-//                            CKEDITOR.config.height = getWysiwygHeight(el);
-//                            CKEDITOR.config.format_tags = getWysiwygFormatTagsOptions(el);
-//                            CKEDITOR.config.on = {
-//                                'instanceReady': function(evt)
-//                                {
-//                                    if (el.id == ID) // set the focus to cloned editor
-//                                    {
-//                                        this.focus();
-//                                    }
-//                                }
-//                            };
-//                            CKEDITOR.replace(el, CKEDITOR.config); // initialize manually with custom config
-//                        });
-//                    }
-                }
-
-                $self.trigger('blur'); // unfocus the button
-                return false;
-            });
 
     <?php
     // SORTING: with custom classes for easier and more generic setup on various elements ?>
     $('.sortable').sortable(
+    {
+        containment: "parent",
+        handle: ".sortable-handle",
+        items: ".sortable-item",
+        axis: "y",
+        update: function(event, ui)
+        {
+
+            var orderFieldId = $(this).data('sortable_field');
+            var $field = $('#'+orderFieldId);
+            if ($field.length)
             {
-                containment: "parent",
-                handle: ".sortable-handle",
-                items: ".sortable-item",
-                axis: "y",
-                update: function(event, ui)
-                {
+                var data = [];
 
-                    var orderFieldId = $(this).data('sortable_field');
-                    var $field = $('#'+orderFieldId);
-                    if ($field.length)
-                    {
-                        var data = [];
+                $(this).find('.sortable-item').each(function(i, el){
+                    data.push($(el).data('sortable_item'));
+                });
 
-                        $(this).find('.sortable-item').each(function(i, el){
-                            data.push($(el).data('sortable_item'));
-                        });
+                // update hidden fields value with updated order
+                $field.val(data.join(','));
+            }
+        },
+        start: function(event, ui)
+        {
+            // force all wysiwyg fields to populate native equivalents
+            for (var i in CKEDITOR.instances)
+            {
+                CKEDITOR.instances[i].updateElement();
+            }
+        },
+        stop: function(event, ui)
+        {
+            // remove all wysiwyg instances
+            for (var i in CKEDITOR.instances)
+            {
+                CKEDITOR.instances[i].destroy();
+            }
 
-                        // update hidden fields value with updated order
-                        $field.val(data.join(','));
-                    }
-                },
-                start: function(event, ui)
-                {
-                    // force all wysiwyg fields to populate native equivalents
-                    for (var i in CKEDITOR.instances)
-                    {
-                        CKEDITOR.instances[i].updateElement();
-                    }
-                },
-                stop: function(event, ui)
-                {
-                    // remove all wysiwyg instances
-                    for (var i in CKEDITOR.instances)
-                    {
-                        CKEDITOR.instances[i].destroy();
-                    }
-
-                    // rebuild all wysiwyg fields
-                    $('.ckeditor').each(function(idx, el)
-                    {
-                        CKEDITOR.config.toolbar = getWysiwygToolbarOptions(el);
-                        CKEDITOR.config.height = getWysiwygHeight(el);
-                        CKEDITOR.config.format_tags = getWysiwygFormatTagsOptions(el);
-                        CKEDITOR.replace(el, CKEDITOR.config);
-                    });
-                }
+            // rebuild all wysiwyg fields
+            $('.ckeditor').each(function(idx, el)
+            {
+                CKEDITOR.config.toolbar = WYSIWYG.getToolbarOptions(el);
+                CKEDITOR.config.height = WYSIWYG.getHeight(el);
+                CKEDITOR.config.format_tags = WYSIWYG.getFormatTagsOptions(el);
+                CKEDITOR.replace(el, CKEDITOR.config);
             });
+        }
+    });
 
 
     <?php
     // FIELD REMOVING: except last one ?>
     $(document).on('click', '.field-remove', function()
-            {
-                var $field;
+    {
+        var $field;
 
-                var $self = $(this);
-                var $inputGroup = $self.parent('.input-group');
+        var $self = $(this);
+        var $inputGroup = $self.parent('.input-group');
 
-                $field = ($inputGroup.length) ? $inputGroup :$self.siblings('.form-control');
+        $field = ($inputGroup.length) ? $inputGroup :$self.siblings('.form-control');
 
-                if (!$field.siblings('.form-control, .input-group').length)
-                {
-                    alert("Can't remove.\nAt least one field instance must be present.");
-                    return false;
-                }
+        if (!$field.siblings('.form-control, .input-group').length)
+        {
+            alert("Can't remove.\nAt least one field instance must be present.");
+            return false;
+        }
 
-                if(doubleCheck(this))
-                {
-                    $field.remove();
-                    return;
-                }
-            });
+        if(doubleCheck(this))
+        {
+            $field.remove();
+            return;
+        }
+    });
 
 
     $('[data-toggle="tooltip"]').tooltip();
