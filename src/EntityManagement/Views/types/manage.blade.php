6e19054d396d68ac65518585cd530ee8dc16a995
Pawel Nowak <pablo.novacco@gmail.com>
2016-05-11 14:25:07 +0100

    Updated admin types listing.

--- src/EntityManagement/Views/types/manage.blade.php
+++ src/EntityManagement/Views/types/manage.blade.php
@@ -13,6 +13,7 @@
             <thead>
             <tr>
                 <th>Name</th>
+                <th>Type</th>
                 <th></th>
             </tr>
             </thead>
@@ -20,6 +21,7 @@
             @foreach ($types->all() as $type)
                 <tr>
                     <td>{{$type->name}}</td>
+                    <td>{{$type->type}}</td>
                     <td>
                         <a href="{{ route('cms:types:edit', ['typeId' => $type->id]) }}" class="btn btn-primary-outline btn-sm">Edit</a>
                         <a href="{{ route('cms:types:delete', ['typeId' => $type->id]) }}" class="btn btn-danger-outline btn-sm confirm">Delete</a>
