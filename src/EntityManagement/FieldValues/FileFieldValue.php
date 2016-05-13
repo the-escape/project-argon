9017689880540028028de3a792f07019a4ff7a6b
Pawel Nowak <pablo.novacco@gmail.com>
2016-05-13 16:42:18 +0100

    Updated File Field Value class to pull media items from media library, not just string id.

--- src/EntityManagement/FieldValues/FileFieldValue.php
+++ src/EntityManagement/FieldValues/FileFieldValue.php
@@ -5,8 +5,10 @@ namespace Escape\Argon\EntityManagement\FieldValues;
 use Escape\Argon\EntityManagement\Eloquent\FieldData;
 use Escape\Argon\Media\Eloquent\MediaItemRepository;
 
-class FileFieldValue extends AbstractFieldValue implements \IteratorAggregate, \Countable
+class FileFieldValue extends AbstractFieldValue implements \Countable, \Iterator
 {
+    private $position;
+
     public function __construct($data = null)
     {
         if ($data == null) {
@@ -16,34 +18,98 @@ class FileFieldValue extends AbstractFieldValue implements \IteratorAggregate, \
         }
 
         parent::__construct($d);
-    }
-
-    /**
-     * Retrieve an external iterator
-     * @link http://php.net/manual/en/iteratoraggregate.getiterator.php
-     * @return Traversable An instance of an object implementing <b>Iterator</b> or
-     * <b>Traversable</b>
-     * @since 5.0.0
-     */
-    public function getIterator()
-    {
-        /** @var MediaItemRepository $itemRepository */
-        $itemRepository = app()->make(MediaItemRepository::class);
-        if (is_array($this->data)) {
-            $data = array_map(
-                function ($id) use ($itemRepository) {
-                    return $itemRepository->find($id);
-                },
-                $this->data
-            );
-        } else {
-            $data = [];
-        }
-        return new \ArrayIterator($data);
+        $this->position = 0;
     }
 
     public function count()
     {
         return count($this->data);
+    }
+
+    /**
+     * Return the current element
+     * @link http://php.net/manual/en/iterator.current.php
+     * @return MediaItem
+     * @since 5.0.0
+     */
+    public function current()
+    {
+        $key = @array_keys($this->data)[$this->position];
+
+        $id = $this->data[$key];
+
+        if ($id) {
+            /** @var MediaItemRepository $itemRepository */
+            $itemRepository = app()->make(MediaItemRepository::class);
+            $media_item = $itemRepository->find($id);
+            $media_item->filesize_formatted = $media_item->getFriendlyFilesize();
+            $media_item->meta = json_decode($media_item->meta);
+            $media_item->data = new \stdClass();
+            return $media_item;
+        }
+        return null;
+    }
+
+    /**
+     * Move forward to next element
+     * @link http://php.net/manual/en/iterator.next.php
+     * @return void Any returned value is ignored.
+     * @since 5.0.0
+     */
+    public function next()
+    {
+        ++$this->position;
+    }
+
+    /**
+     * Return the key of the current element
+     * @link http://php.net/manual/en/iterator.key.php
+     * @return mixed scalar on success, or null on failure.
+     * @since 5.0.0
+     */
+    public function key()
+    {
+        return $this->position;
+    }
+
+    /**
+     * Checks if current position is valid
+     * @link http://php.net/manual/en/iterator.valid.php
+     * @return boolean The return value will be casted to boolean and then evaluated.
+     * Returns true on success or false on failure.
+     * @since 5.0.0
+     */
+    public function valid()
+    {
+        return array_key_exists($this->position, array_keys($this->data));
+    }
+
+    /**
+     * Rewind the Iterator to the first element
+     * @link http://php.net/manual/en/iterator.rewind.php
+     * @return void Any returned value is ignored.
+     * @since 5.0.0
+     */
+    public function rewind()
+    {
+        $this->position = 0;
+    }
+
+    public function getUrl()
+    {
+        if ($this->current()) {
+            return $this->current()->getUrl();
+        }
+
+        return null;
+    }
+
+    public function isEmpty()
+    {
+        if ($this->current()) {
+            return false;
+        }
+
+        return true;
     }
 }
