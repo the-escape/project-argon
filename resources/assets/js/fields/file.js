e1976c83c334afdd40871e22a7c2a9ef628dc6d5
Ben Perkins <ben.perkins@the-escape.co.uk>
2016-10-03 14:01:33 +0100

    Changed the media folder DIR config.

--- resources/assets/js/fields/file.js
+++ resources/assets/js/fields/file.js
@@ -113,7 +113,7 @@ $(document).on('click', '.field-file .field-add-file', function(e) {
         var field = $(e.target).closest('.field');
 
         $.ajax(
-            '../../../media/items/' +selected
+            argon.root() + '/media/items/' + selected
         ).done(function(data) {
 
             var settings = JSON.parse(field.attr('data-settings'));
@@ -147,7 +147,7 @@ $(document).on('click', '.field-image .field-add-file', function(e) {
         var field = $(e.target).closest('.field');
 
         $.ajax(
-            '../../../media/items/' +selected
+            argon.root() + '/media/items/' + selected
         ).done(function(data) {
             var guid = new Date().valueOf();
             var settings = JSON.parse(field.attr('data-settings'));
