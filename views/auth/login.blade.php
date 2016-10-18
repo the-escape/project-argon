6a091eb4f2ac5cdb923891e7f0f1c116fa37707e
Pawel Nowak <pablo.novacco@gmail.com>
2016-10-18 11:31:41 +0100

    Updated admin styles for navbar and logo.

--- views/auth/login.blade.php
+++ views/auth/login.blade.php
@@ -8,6 +8,52 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="/argon/css/app.css">
     <style>
+        .navbar.bg-inverse {
+            background-color: {{config('argon.neutral_color', '#373a3c')}};
+        }
+
+        a,
+        .nav-link,
+        .btn-link,
+        .btn-primary-outline {
+            color: {{config('argon.highlight_color', '#0275d8')}};
+        }
+
+        .btn-primary {
+            background-color: {{config('argon.highlight_color', '#0275d8')}};
+        }
+
+        .btn-primary,
+        .btn-primary-outline {
+            border-color: {{config('argon.highlight_color', '#0275d8')}};
+        }
+
+        .btn-primary-outline:hover,
+        .btn-primary-outline:focus,
+        .btn-primary-outline:active,
+        .btn-primary:hover,
+        .btn-primary:focus,
+        .btn-primary:active,
+        .open .btn-primary-outline.dropdown-toggle {
+            background-color: {{config('argon.highlight_color_darker', config('argon.highlight_color', '#025aa5'))}};
+        }
+
+        .btn-primary-outline:hover,
+        .btn-primary-outline:focus,
+        .btn-primary-outline:active,
+        .btn-primary:hover,
+        .btn-primary:focus,
+        .btn-primary:active,
+        .open .btn-primary-outline.dropdown-toggle {
+            border-color: {{config('argon.highlight_color_darker', config('argon.highlight_color', '#025aa5'))}};
+        }
+
+        a:hover,
+        .btn-link:hover,
+        .btn-link:active,
+        .btn-link:focus {
+            color: {{config('argon.highlight_color_darker', config('argon.highlight_color', '#025aa5'))}};
+        }
 
         .logo-admin-login {
             {{config('argon.logo-admin-login')}}
