a2a8791edd69648cf807c49c9b1d746a8966bb9f
Pawel Nowak <pablo.novacco@gmail.com>
2016-10-28 15:40:33 +0100

    Updated frontent page - removed buggy magic methods.

--- src/Frontend/Page.php
+++ src/Frontend/Page.php
@@ -18,40 +18,12 @@ class Page
 
     protected $revisionId;
 
-    protected $custom = [];
-
     public function __construct(Entity $entity, Request $request=null)
     {
         $this->entity = $entity;
         $this->request = isset($request) ? $request : app()->make('\Escape\Argon\Core\Http\Request');
         $this->revisionId = $this->isPreview();
     }
-
-
-    public function __set($name, $value)
-    {
-        $this->custom[$name] = $value;
-    }
-
-    public function __get($name)
-    {
-        if (array_key_exists($name, $this->custom)) {
-            return $this->custom[$name];
-        }
-
-        throw new RuntimeException("Trying to access undefined property '{$name}' via __get() in " . __METHOD__);
-    }
-
-    public function __isset($name)
-    {
-        return isset($this->custom[$name]);
-    }
-
-    public function __unset($name)
-    {
-        unset($this->custom[$name]);
-    }
-
 
     public function isPreview()
     {
