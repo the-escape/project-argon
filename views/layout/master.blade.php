aabed616a0bc0312f011c48014ccb7869838872f
Ben Perkins <b.c.m.perkins@gmail.com>
2017-01-05 11:34:42 +0000

    Sidebar changes.

--- views/layout/master.blade.php
+++ views/layout/master.blade.php
@@ -68,6 +68,23 @@
                         </div>
                     </div>
                 </div>
+                <div class="main">
+                    <div class="container-fluid bg-grey">
+                        <div class="row">
+                            <div class="col-xs-10 col-xs-offset-1">
+                                <div class="tabs">
+                                    <div class="tabs__header">
+                                        <h1>Sitemap</h1>
+                                        <ul class="tabs__items">
+                                            <li class="tabs__item"><a href="#" class="active">PAGES</a></li>
+                                            <li class="tabs__item"><a href="#">REVISIONS</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         <script type="application/javascript" src="{{ asset('argon/assets/js/libs.min.js') }}"></script>
