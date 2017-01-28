0f4e69d093b5f8265e06ccd46776023b89e8bf0d
Pawel Nowak <pablo.novacco@gmail.com>
2017-01-28 23:25:59 +0000

    Medialib updates - create page

--- src/EntityManagement/Views/pages/edit.blade.php
+++ src/EntityManagement/Views/pages/edit.blade.php
@@ -240,27 +240,7 @@
         </div>
     </div>
 
-    <div id="medialib" class="modal fade" role="dialog" aria-labelledby="medialibraryLabel" aria-hidden="true">
-        <input type="hidden" id="selectedMediaItem" value="">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                    <h4 class="modal-title" id="medialibraryLabel">Media Library</h4>
-                </div>
-                <div class="modal-body">
-
-                    <div class="media-library" style="position: relative;">
-
-                        <iframe id="medialib-iframe" frameborder="0"></iframe>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    @include('argon::pages.partials.medialib')
 
     <div style="display: none;" id="preview-template">
         <div class="media-item">
