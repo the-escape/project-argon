c49f45134e38d76b489d0465135fbb7c0eab5661
Ben Perkins <b.c.m.perkins@gmail.com>
2017-02-21 17:45:41 +0000

    Folder nesting is now styled.

--- resources/assets/js/components/Media.vue
+++ resources/assets/js/components/Media.vue
@@ -30,19 +30,19 @@
                     </form>
                     <div class="media__actions">
                         <h1 v-if="!searchFilter.length">{{ activeFolder.name }}</h1>
-                        <h1 v-else>Search: {{ searchQuery }}</h1>
+                        <h1 v-else>Search: {{ searchFilter }}</h1>
                         <button class="form__btn form__btn--small media__upload">UPLOAD MEDIA</button>
                     </div>
                 </div>
                 <div class="media__browser">
                     <div class="media__items">
-                        <div class="media__item media__item--folder" v-on:click="setCreating">
+                        <div class="media__item media__item--folder" v-on:click="setCreating" v-if="!searchFilter.length">
                             <div class="media__asset media__asset--folder">
                                 <img src="/argon/assets/img/icons/folder-add.png" width="100%" alt="add">
                             </div>
                             <span class="media__name">New folder</span>
                         </div>
-                        <div class="media__item media__item--folder" v-if="isCreating">
+                        <div class="media__item media__item--folder" v-if="isCreating && !searchFilter.length">
                             <div class="media__asset media__asset--folder">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 85.04 56.69"><path d="M79.3 9.77H42.2a5.41 5.41 0 0 1-3.56-1.33L29.88.76a3.1 3.1 0 0 0-2-.76H3.54A3.49 3.49 0 0 0 0 3.42v47.73a5.66 5.66 0 0 0 5.74 5.54H79.3a5.64 5.64 0 0 0 5.7-5.54V15.32a5.64 5.64 0 0 0-5.7-5.55z"/></svg>
                             </div>
