f666473ea0bac563d1f43a16b0966647d0af9c09
Franco Schembre <franco.schembre@the-escape.co.uk>
2017-09-07 16:21:45 +0100

    Tidy up

--- config/argon.php
+++ config/argon.php
@@ -2,22 +2,58 @@
 
 return [
     'admin_route_prefix' => '/admin',
-    'client_logo_dark' => '/assets/img/logo-admin.png',
-    'client_logo_light' => '/assets/img/logo-admin.png',
-    'highlight_color' => '#009baa',
-    'highlight_color_darker' => '#ff7b09',
-    'neutral_color' => '#3F5463',
-    'logo-admin-login' => 'width:auto; margin-bottom: 30px; max-width:100%;',
-    'navbar' => 'padding-left: 0;padding-top: 0;padding-bottom: 0;height: 51px;',
-    'navbar-nav' => 'height:51px;',
-    'nav-item' => 'height:51px;',
-    'nav-link' => 'line-height:51px; padding-top:0; padding-bottom:0;',
-    'navbar-brand' => 'padding:0;margin:0;',
-    'logo-admin' => 'height:30px; padding:0; margin:10px;',
+    'client_logo_dark' => '/argon/images/logo.png',
+    'client_logo_light' => '/argon/images/logo.png',
+
+    'admin_css' => '
+        .argon-login h1 { background-color: #fff; }
+
+        .navbar.bg-inverse { background-color: #373a3c; }
+
+        a,
+        .nav-link,
+        .btn-link,
+        .btn-primary-outline { color: #0275d8; }
+
+        .btn-primary { background-color: #0275d8; }
+
+        .btn-primary,
+        .btn-primary-outline { border-color: #0275d8; }
+
+        .btn-primary-outline:hover,
+        .btn-primary-outline:focus,
+        .btn-primary-outline:active,
+        .btn-primary:hover,
+        .btn-primary:focus,
+        .btn-primary:active,
+        .open .btn-primary-outline.dropdown-toggle { background-color: #025aa5; }
+
+        .btn-primary-outline:hover,
+        .btn-primary-outline:focus,
+        .btn-primary-outline:active,
+        .btn-primary:hover,
+        .btn-primary:focus,
+        .btn-primary:active,
+        .open .btn-primary-outline.dropdown-toggle { border-color: #025aa5; }
+
+        a:hover,
+        .btn-link:hover,
+        .btn-link:active,
+        .btn-link:focus { color: #025aa5; }
+
+        .logo-admin-login { width:auto; margin-bottom:auto; max-width:100%; }
+
+        .navbar { padding-left:0; padding-top:0; padding-bottom:0; height:51px; }
+        .navbar .navbar-nav { height:51px; }
+        .navbar .navbar-nav .nav-item { height:51px; }
+        .navbar .navbar-nav .nav-link { line-height:51px; padding-top:0; padding-bottom:0; }
+        .navbar .navbar-brand { padding:0; margin:0; }
+        .navbar .navbar-brand .logo-admin { height:51px; padding:0; margin:0; }
+    ',
 
     'sitemap_view' => 'argon::pages.sitemap',
 
-    'client_name' => 'Sentinel Housing',
+    'client_name' => '',
 
     /*
      * Options for jstree open on load:
@@ -27,19 +63,14 @@ return [
      * */
     'jstree' => [
         'load' => [
-            'open' => " 'open_node', ['#node-1'] ",
+            'open' => " 'open_all' ",
         ]
     ],
 
-    'views' => [
-        1 => 'pages.home',
-        2 => 'pages.blocks',
-        4 => 'pages.news',
-        5 => 'pages.news-single',
-        12 => 'pages.search',
-        13 => 'pages.sitemap',
-        14 => 'pages.property',
-        15 => 'pages.development-listing',
-        16 => 'pages.development',
-    ],
+    // content type Id to view template mapping
+    'views' => [],
+
+    'medialibrary' => [
+        'perpage' => 20,
+    ]
 ];
