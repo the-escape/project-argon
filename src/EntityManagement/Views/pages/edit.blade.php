0196662e3211cc4227475b7cfb65ff420a8e8c6a
Franco Schembre <franco.schembre@the-escape.co.uk>
2017-09-15 14:03:16 +0100

    Conditional to seo and theme options display

--- src/EntityManagement/Views/pages/edit.blade.php
+++ src/EntityManagement/Views/pages/edit.blade.php
@@ -200,7 +200,15 @@ $localisedFrontEndPageUrl = $pageLocaleSlug.$defaultFronEndPageUrl;
                     <div class="card accordion">
 
                         <div class="card-header accordion-header">
-
+                            @if($group->hasImage())
+                                <div class="thumbnail">
+                                    <a href="{{ $group->getImage() }}">
+                                        <img src="{{ $group->getImage() }}">
+                                    </a>
+                                    <input type="hidden" class="url" value="{{ $group->getImage() }}">
+                                </div>
+                                <?php $atleastOneGroupImage=true;?>
+                            @endif
                             {{ $group->name }}
 
                             @if($group->isRenderable())
