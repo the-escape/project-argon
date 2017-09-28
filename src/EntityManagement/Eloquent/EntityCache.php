c31311721aa403c78b319b736eea9b107ca5ef49
Pawel Nowak <pablo.novacco@gmail.com>
2017-09-28 13:21:35 +0100

    EntityCache -> added methods to getters to get cached entities properties.

--- src/EntityManagement/Eloquent/EntityCache.php
+++ src/EntityManagement/Eloquent/EntityCache.php
@@ -439,6 +439,46 @@ class EntityCache extends Model implements Compressable
         return $this->entity_url;
     }
 
+    public function getName()
+    {
+        return $this->entity_name;
+    }
+
+    public function getSlug()
+    {
+        return $this->entity_slug;
+    }
+
+    public function getLocalisationId()
+    {
+        return $this->entity_localisation_id;
+    }
+
+    public function getLocaleId()
+    {
+        return $this->entity_locale_id;
+    }
+
+    public function getTypeId()
+    {
+        return $this->entity_type_id;
+    }
+
+    public function getTypeType()
+    {
+        return $this->entity_type_type;
+    }
+
+    public function getParentId()
+    {
+        return $this->entity_parent_id;
+    }
+
+    public function getStatus()
+    {
+        return $this->entity_status;
+    }
+
     public function findForPath($url=null, $status=1, $trigger404=true)
     {
         if (is_null($url))
