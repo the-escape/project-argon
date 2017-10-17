4b48af46beb0d2c344adb3ba3025eb6de5b65afa
Pawel Nowak <pablo.novacco@gmail.com>
2017-10-17 12:18:23 +0100

    Updated redirect manager.

--- src/RedirectManagement/Middleware/RedirectMiddleware.php
+++ src/RedirectManagement/Middleware/RedirectMiddleware.php
@@ -13,20 +13,56 @@ class RedirectMiddleware
     }
 
     /**
-     * Handle an incoming request.
+     * Filter an incoming request against list of redirects.
+     * NOTE: You can pass multiple exclude parameters as a comma-separated list
+     * NOTE: You can exclude parameter(s) can end with * - wildcard
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
+     * @param  \Closure  $next you can also pass multiple parameters as a comma-separated list - avoid whitespace!
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle($request, Closure $next, $exclude)
     {
+        $REQUEST_URI = filter_input(INPUT_SERVER, 'REQUEST_URI');
+
+        if (is_null($REQUEST_URI))
+        {
+            return $next($request);
+        }
+
+        $path = parse_url($REQUEST_URI, PHP_URL_PATH);
+        $excludes = array_filter(explode(',', $exclude), 'strlen');
+
+        foreach ($excludes as $exclude)
+        {
+            if ($path == $exclude)
+            {
+                return $next($request);
+            }
+
+            $last = $exclude[strlen($exclude)-1];
+
+            if ($last == '*')
+            {
+                $exclude = rtrim($exclude,'*');
+                $strpos = strpos($path, $exclude);
+
+                if ($strpos === 0)
+                {
+                    return $next($request);
+                }
+            }
+        }
+
         $redirectRepository = app()->make(RedirectRepository::class);
         $redirects = $redirectRepository->all();
 
         foreach ($redirects as $redirect)
         {
-            if ($redirect->from == $request->getPathInfo())
+            $from = $redirect->from;
+
+            if ($from == $path)
             {
                 return redirect($redirect->to, 301);
             }
