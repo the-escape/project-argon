d5257ec27868f0d3b700bb82bcdf3ade099aa7e4
Lee Martin <lee.martin@the-escape.co.uk>
2018-09-07 11:14:44 +0100

    adding localisation to menus - fixed validation rules

--- src/Menus/Controllers/MenusController.php
+++ src/Menus/Controllers/MenusController.php
@@ -78,7 +78,7 @@ class MenusController extends BaseController
 
         $rules = [
             'name' => "required",
-            'slug' => "required|unique:menus,slug,{$id},id,deleted_at,NULL,{$request->input('locale_id')}",
+            'slug' => "required|unique:menus,slug,{$id},id,deleted_at,NULL,locale_id,{$request->input('locale_id')}",
             'menu' => 'required', // perhaps validate json?
         ];
 
