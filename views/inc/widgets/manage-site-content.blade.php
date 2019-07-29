a59622a27e065076ab97437478647d2c7e554c35
Tomasz Kwiatkowski <tomasz.kwiatkowski@the-escape.co.uk>
2019-07-29 15:32:41 +0100

    change in widget

--- views/inc/widgets/manage-site-content.blade.php
+++ views/inc/widgets/manage-site-content.blade.php
@@ -1,4 +1,4 @@
-<div class="c-widget c-widget--background-image" style="background-image: url('{{ $bgImage or '' }}')">
+<div class="c-widget c-widget--background-image" style="background-image: url('{{ $bgImage ?? '' }}')">
     <div class="c-widget__btn-cover c-widget--centered">
         <a class="o-btn o-btn--white-outline" href="{{ route('cms:pages:manage') }}">Manage site content</a>
     </div>
