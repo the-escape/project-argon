911c85c57184c99ea8a2426a2fe6f50aab8a7b4e
Tomasz Kwiatkowski <tomasz.kwiatkowski@the-escape.co.uk>
2019-07-29 13:42:03 +0100

    updates to migrations

--- src/Locales/Migrations/2015_09_17_084227_create_locales_table.php
+++ src/Locales/Migrations/2015_09_17_084227_create_locales_table.php
@@ -28,7 +28,7 @@ class CreateLocalesTable extends Migration
         $locale = new Locale();
         $locale->name = 'Global';
         $locale->languageCode = 'en_GB';
-        $locale->languageCode = '';
+        $locale->locale_slug = '';
         $locale->region = 'Global';
         $locale->save();
 
