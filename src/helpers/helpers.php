e6f552cb09f47057c8aba8f18a423099ef9780ae
Tomasz Kwiatkowski <tomasz.kwiatkowski@the-escape.co.uk>
2019-07-30 14:45:41 +0100

    reorganised version checks

--- src/helpers/helpers.php
+++ src/helpers/helpers.php
@@ -5,12 +5,9 @@
  *
  * @return string
  */
-if (!function_exists('laravelVersion'))
+function laravelVersion()
 {
-    function laravelVersion()
-    {
-        return App::VERSION();
-    }
+    return App::VERSION();
 }
 
 /**
@@ -18,12 +15,9 @@ if (!function_exists('laravelVersion'))
  *
  * @return boolean
  */
-if (!function_exists('isLaravelVersionPre'))
+function isLaravelVersionPre($version)
 {
-    function isLaravelVersionPre($version)
-    {
-        return version_compare(laravelVersion(), $version, "lt");
-    }
+    return version_compare(laravelVersion(), $version, "lt");
 }
 
 /**
@@ -31,58 +25,7 @@ if (!function_exists('isLaravelVersionPre'))
  *
  * @return boolean
  */
-if (!function_exists('isLaravelVersionPost'))
+function isLaravelVersionPost($version)
 {
-    function isLaravelVersionPost($version)
-    {
-        return version_compare(laravelVersion(), $version, "gt");
-    }
-}
-
-/**
- * IE11 check
- *
- * @return boolean
- */
-if (!function_exists('isIE11'))
-{
-    function isIE11()
-    {
-        $userAgent = request()->header('User-Agent', '');
-
-        return strpos($userAgent, 'Trident/7.0; rv:11.0') !== false;
-    }
-}
-
-/**
- * Checks if logged in user is an Admin
- *
- * @return boolean
- */
-if (!function_exists('isAdmin'))
-{
-    function isAdmin()
-    {
-        $user = Auth::user();
-        return $user ? $user->hasRole('Admin') : false;
-    }
-}
-
-/**
- * Checks if the user is from The Escape location or using local environment
- *
- * @return boolean
- */
-if (!function_exists('isEscape'))
-{
-    function isEscape()
-    {
-        $escape_ips = [
-            '127.0.0.1',
-            '192.168.10.1', // homestead host machine
-            '185.74.236.118' // escape office
-        ];
-
-        return in_array($_SERVER["REMOTE_ADDR"], $escape_ips);
-    }
+    return version_compare(laravelVersion(), $version, "gt");
 }
