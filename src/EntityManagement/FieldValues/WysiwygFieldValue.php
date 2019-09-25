effb4a22bafe46e5c7b73779bbb0011de630170b
Tomasz Kwiatkowski <tomasz.kwiatkowski@the-escape.co.uk>
2019-09-25 11:20:44 +0100

    hotifx for wysiwg field

--- src/EntityManagement/FieldValues/WysiwygFieldValue.php
+++ src/EntityManagement/FieldValues/WysiwygFieldValue.php
@@ -4,6 +4,16 @@ namespace Escape\Argon\EntityManagement\FieldValues;
 
 class WysiwygFieldValue extends TextFieldValue
 {
+    public function __construct($data = null)
+    {
+        if (is_object($data))
+        {
+            $data = (array) $data;
+        }
+
+        parent::__construct($data);
+    }
+
     public function __toString()
     {
         return parent::__toString();
