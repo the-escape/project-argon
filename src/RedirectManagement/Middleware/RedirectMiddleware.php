03d944aba3af2791c2e627973d296f3669c34258
Adam Frame <adam.frame@the-escape.co.uk>
2020-10-05 09:15:51 +0000

    Trim spaces from values

--- src/RedirectManagement/Middleware/RedirectMiddleware.php
+++ src/RedirectManagement/Middleware/RedirectMiddleware.php
@@ -7,7 +7,6 @@ use Escape\Argon\RedirectManagement\Eloquent\RedirectRepository;
 
 class RedirectMiddleware
 {
-
     public function __construct()
     {
     }
@@ -15,19 +14,18 @@ class RedirectMiddleware
     /**
      * Filter an incoming request against list of redirects.
      * NOTE: You can pass multiple exclude parameters as a comma-separated list
-     * NOTE: You can exclude parameter(s) can end with * - wildcard
+     * NOTE: You can exclude parameter(s) can end with * - wildcard.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string $exclude - parameters as a comma-separated list - avoid whitespace!
+     * @param \Illuminate\Http\Request $request
+     * @param string                   $exclude - parameters as a comma-separated list - avoid whitespace!
+     *
      * @return mixed
      */
-    public function handle($request, Closure $next, $exclude=null)
+    public function handle($request, Closure $next, $exclude = null)
     {
         $REQUEST_URI = filter_input(INPUT_SERVER, 'REQUEST_URI');
 
-        if (is_null($REQUEST_URI))
-        {
+        if (is_null($REQUEST_URI)) {
             return $next($request);
         }
 
@@ -35,27 +33,22 @@ class RedirectMiddleware
 
         $excludes = [];
 
-        if (!is_null($exclude))
-        {
+        if (!is_null($exclude)) {
             $excludes = array_filter(explode(',', $exclude), 'strlen');
         }
 
-        foreach ($excludes as $exclude)
-        {
-            if ($path == $exclude)
-            {
+        foreach ($excludes as $exclude) {
+            if ($path == $exclude) {
                 return $next($request);
             }
 
-            $last = $exclude[strlen($exclude)-1];
+            $last = $exclude[strlen($exclude) - 1];
 
-            if ($last == '*')
-            {
-                $exclude = rtrim($exclude,'*');
+            if ('*' == $last) {
+                $exclude = rtrim($exclude, '*');
                 $strpos = strpos($path, $exclude);
 
-                if ($strpos === 0)
-                {
+                if (0 === $strpos) {
                     return $next($request);
                 }
             }
@@ -64,13 +57,11 @@ class RedirectMiddleware
         $redirectRepository = app()->make(RedirectRepository::class);
         $redirects = $redirectRepository->all();
 
-        foreach ($redirects as $redirect)
-        {
-            $from = $redirect->from;
+        foreach ($redirects as $redirect) {
+            $from = trim($redirect->from);
 
-            if ($from == $path)
-            {
-                return redirect($redirect->to, 301);
+            if ($from == trim($path)) {
+                return redirect(trim($redirect->to), 301);
             }
         }
 
