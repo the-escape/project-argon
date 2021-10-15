1f544d6a1ecc47eb1ec0df434fe499889b4df5da
Adam Frame <adam.frame@the-escape.co.uk>
2021-10-15 14:35:26 +0000

    Upgrade to 5.4

--- src/Core/ArgonServiceProvider.php
+++ src/Core/ArgonServiceProvider.php
@@ -15,7 +15,7 @@ use Illuminate\Support\ServiceProvider;
 use Intervention\Image\Facades\Image;
 use Intervention\Image\ImageServiceProvider;
 use Prettus\Repository\Providers\RepositoryServiceProvider;
-use Maknz\Slack\Laravel\ServiceProvider as SlackServiceProvider;
+use Escape\Argon\Libs\Slack\ServiceProvider as SlackServiceProvider;
 use Maknz\Slack\Facades\Slack;
 use Illuminate\Foundation\AliasLoader;
 
@@ -68,7 +68,7 @@ class ArgonServiceProvider extends ServiceProvider
         $this->app->register(ImageServiceProvider::class);
         if (!class_exists("Image"))
         {
-            class_alias(Image::class, 'Image');
+            //class_alias(Image::class, 'Image');
         }
 
         $this->app->register(MediaServiceProvider::class);
@@ -81,6 +81,7 @@ class ArgonServiceProvider extends ServiceProvider
 
         // this is how we load the facade in the service provider
         $loader = AliasLoader::getInstance();
-        $loader->alias('Slack', 'Maknz\Slack\Laravel\Facade');
+        $loader->alias('Slack', 'Escape\Argon\Libs\Slack\Facade');
+        $loader->alias('Image', 'Intervention\Image\Facades\Image');
     }
 }
